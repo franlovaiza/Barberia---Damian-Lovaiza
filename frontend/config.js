77d1ccb0b6e6c1a9c97e4fff060c28f59a06f248
franlovaiza <franlovaiza@gmail.com>
2025-12-27 19:08:03 -0300

    agregar conexion front y back

--- frontend/config.js
+++ frontend/config.js
@@ -1,1 +1,9 @@
-export const API_URL='http://localhost:8000'
+//export const API_URL='http://localhost:8000'
+
+// config.js
+const isLocal = window.location.hostname === 'localhost' || 
+                window.location.hostname === '127.0.0.1';
+
+export const API_URL = isLocal 
+    ? 'http://localhost:8000' 
+    : 'https://barberia-lovaiza.onrender.com';  
