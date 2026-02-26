c9f7d9f6ba1829a1f6d5c3b63526df1bc9627905
franlovaiza <franlovaiza@gmail.com>
2026-02-26 19:09:38 -0300

    fix: apuntar API a Render

--- backend/db/init_db.py
+++
@@ -1,6 +0,0 @@
-from .database import Base, engine
-from .models.turno_model import Turno
-
-def init_db():  # Quitar async
-    Base.metadata.create_all(bind=engine)
-    print("Base de datos inicializada")  # Para confirmar que se ejecutó
